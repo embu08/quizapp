24dd968232ccb5d318f6a57541ed6e5027bc58a0
embu08 <a800x800@gmail.com>
2022-10-10 22:10:18 +0300

    an attempt to figure out how to write tests

--- quizapp/main_app/tests/test_models.py
+++ quizapp/main_app/tests/test_models.py
@@ -1,30 +1,22 @@
-from django.template.backends import django
 from django.test import TestCase
-from ..models import *
-from django.contrib.auth import get_user_model
+from main_app.models import *
+from django.db.utils import IntegrityError
 
 
-class CategoriesModelTest(TestCase):
+class QuestionsModelTestCase(TestCase):
 
-    def test_create_category(self):
-        cat = Categories.objects.create(name='Test')
-        self.assertEqual(cat.name, 'Test')
-        self.assertEqual(len(Categories.objects.all()), 1)
+    def setUp(self):
+        self.t = Test.objects.create(name='Test')
+        self.q1 = Questions.objects.create(
+            question='why',
+            correct_answer='because',
+            answer_1='idk',
+            test=self.t)
 
-    def test_delete_category(self):
-        Categories.objects.create(name='Test')
-        Categories.objects.filter(name='Test').delete()
-        self.assertEqual(len(Categories.objects.all()), 0)
-
-
-class TestTest(TestCase):
-    def test_create_test(self):
-        cat = Categories.objects.create(name='Test')
-        User = get_user_model()
-        user = User.objects.create_user(username='user123', password='foo')
-        test_1 = Test.objects.create(name='My test', owner=user, category=cat)
-        self.assertEqual(test_1.name, 'My test')
-        self.assertEqual(test_1.owner.username, 'user123')
-        self.assertEqual(test_1.category.name, 'Test')
-        test_2 = Test.objects.create(name='My test2', category=cat)
-        self.assertEqual(test_2.owner, None)
+    def test_question_is_already_in_the_test(self):
+        q2 = Questions(question='why',
+                       correct_answer='123',
+                       answer_1='321',
+                       test=self.t)
+        with self.assertRaises(IntegrityError):
+            q2.save()
